7552e30b7a0140b6ff6dd55df42c235070c15212
Mark Roberts <markro@cs.washington.edu>
2017-11-27 13:05:32 -0800

    more updates - system now working on jgrapht and plume-lib

--- coverage.sh
+++ coverage.sh
@@ -2,14 +2,15 @@
 root=`pwd`
 tools=$root/libs
 evalpath=$root/evaluation
-projectpath=$root/integration-test2
+projectpath=$root/toradocu
 projectlibs=$projectpath/libs
 classpath=$root/extractcoverage/build/libs/extractcoverage-all.jar
-corpuspath=$projectpath/corpus
+corpuspath=$projectpath
 coveragepath=$evalpath/coverage
 logpath=$evalpath/logs
 agentpath=$tools/jacocoagent.jar
 outputpath=$coveragepath/
 junitpath=$projectlibs/junit-4.12.jar
 replacecallpath=$projectlibs/replacecall.jar
+echo java -jar $classpath --corpusDirectoryPath $corpuspath --jacocoAgentPath $agentpath --outputPath $outputpath --junitPath $junitpath --replacecallAgentPath $replacecallpath
 java -jar $classpath --corpusDirectoryPath $corpuspath --jacocoAgentPath $agentpath --outputPath $outputpath --junitPath $junitpath --replacecallAgentPath $replacecallpath &> $logpath/coverage-log.txt
